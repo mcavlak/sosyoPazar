7c506150a8b065cd8352ed74d01fc6eda3f305bf
mustafa <80627473+mcavlak@users.noreply.github.com>
2022-04-21 08:49:46 +0300

    improve discovercard

--- frontend/src/components/DiscoverCard.js
+++ frontend/src/components/DiscoverCard.js
@@ -1,24 +1,25 @@
-import { Card, Avatar, Typography, Divider, Button } from '@mui/material'
+import { Card, Avatar, Typography, Divider } from '@mui/material'
 import { Box } from '@mui/system'
+import moment from 'moment'
 import React from 'react'
 
-const DiscoverCard = (props) => {
+const DiscoverCard = ({ postData }) => {
 
     return (
         <Card sx={{ p: 3, boxShadow: "0px 4px 24px rgba(0, 0, 0, 0.1)", borderRadius: "20px" }}>
             <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
                 <Box sx={{ display: "flex", alignItems: "center" }}>
-                    <Avatar sx={{ width: 56, height: 56 }} />
+                    <Avatar sx={{ width: 42, height: 42 }} />
                     <Box sx={{ ml: 2 }}>
-                        <Typography variant='h6' sx={{ margin: 0 }}>Test</Typography>
-                        <Typography variant='p' sx={{ margin: 0, fontWeight: 300 }}>14.12.2021 21.46</Typography>
+                        <Typography variant='h6' sx={{ margin: 0, fontSize: '1.1rem' }}>{postData?.contentOwner?.storeName}</Typography>
+                        <Typography variant='p' sx={{ margin: 0, fontWeight: 300 }}>{moment(postData?.createdDateTime).startOf().fromNow()}</Typography>
                     </Box>
                 </Box>
-                <Button sx={{ justifySelf: "end" }}>TEKLİF VER</Button>
+                {/* <Button sx={{ justifySelf: "end" }}>TEKLİF VER</Button> */}
             </Box>
             <Divider sx={{ my: 2 }} />
             <Typography variant='p'>
-                Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.
+                {postData?.content}
             </Typography>
         </Card >
     )
